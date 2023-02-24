93037a8714ecef7674b9f5e007b19e54e2debc1d
ANGIE YERALDINE HIGUITA DAVID <est.angiehiguita@tecoc.edu.co>
2023-02-24 10:26:07 -0500

    first commit

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+//Cambiar imagen de fondo
+
+// const menu = document.querySelector('#menu');
+
+// const btnNext = document.querySelector('.next');
+// const btnPrev = document.querySelector('.prev');
+
+// let img = ['desktop-image-hero-1.jpg', 'desktop-image-hero-2.jpg', 'desktop-image-hero-3.jpg'];
+
+// let i = 0;
+// let iPrev = 0;
+
+// btnNext.addEventListener('click', () =>{
+//   iPrev = i;
+//   i = (i + 1) % img.length;
+//   menu.classList.add("fade-in");
+//   menu.style.backgroundImage = "url('../images/" + img[i] + "')";
+
+//   setTimeout(function() {
+//     menu.classList.remove("active");
+//   }, 0);
+
+//   setTimeout(function() {
+//     menu.classList.add("active");
+//   }, 300);
+// });
+
+// btnPrev.addEventListener('click', () =>{
+//   iPrev = i;
+//   i = (i - 1 + img.length) % img.length;
+//   menu.classList.add("fade-in");
+//   menu.style.backgroundImage = "url('../images/" + img[i] + "')";
+//   setTimeout(function() {
+//     menu.classList.remove("active");
+//   }, 0);
+
+//   setTimeout(function() {
+//     menu.classList.add("active");
+//   }, 400);
+// });
